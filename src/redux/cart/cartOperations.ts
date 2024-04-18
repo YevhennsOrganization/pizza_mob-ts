7b0d6eb2c81cc516d35faa0505bde6e222346812
Yevhenns <gifestum@gmail.com>
2024-04-18 18:47:40 +0300

    refresh

--- src/redux/cart/cartOperations.ts
+++ src/redux/cart/cartOperations.ts
@@ -4,18 +4,18 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 const BASE_URL = process.env.EXPO_PUBLIC_BASE_URL;
 
 export const sendOrder = createAsyncThunk<
-  number,
-  TSummaryOrder,
-  {
-    rejectValue: string;
-  }
+    number,
+    TSummaryOrder,
+    {
+        rejectValue: string;
+    }
 >("cart/sendOrder", async (order, { rejectWithValue }) => {
-  try {
-    const res = await axios.post(`${BASE_URL}send_email`, {
-      body: JSON.stringify(order),
-    });
-    return res.status;
-  } catch (error: any) {
-    return rejectWithValue(error.message as string);
-  }
+    try {
+        const res = await axios.post(`${BASE_URL}/api/send_email`, {
+            body: JSON.stringify(order),
+        });
+        return res.status;
+    } catch (error: any) {
+        return rejectWithValue(error.message as string);
+    }
 });
--- src/redux/products/productsOperations.ts
+++ src/redux/products/productsOperations.ts
@@ -4,16 +4,16 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 const BASE_URL = process.env.EXPO_PUBLIC_BASE_URL;
 
 export const getProducts = createAsyncThunk<
-  TProductsArr,
-  void,
-  {
-    rejectValue: string;
-  }
+    TProductsArr,
+    void,
+    {
+        rejectValue: string;
+    }
 >("products/getProductsAll", async (_, { rejectWithValue }) => {
-  try {
-    const res = await axios.get(`${BASE_URL}products`);
-    return res.data.data.result;
-  } catch (error: any) {
-    return rejectWithValue(error.message);
-  }
+    try {
+        const res = await axios.get(`${BASE_URL}/api/products`);
+        return res.data.data.result;
+    } catch (error: any) {
+        return rejectWithValue(error.message);
+    }
 });
